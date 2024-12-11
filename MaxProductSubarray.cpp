105416e50a35a6abbffd7e42ca14b34406448b62
Meghna Singh <meghnasingh@Meghnas-MacBook-Air.local>
2024-12-11 12:06:29 +0530

    alternative approach

--- MaxProductSubarray.cpp
+++ MaxProductSubarray.cpp
@@ -17,4 +17,23 @@ public:
 	return max_product;
         
     }
+}
+/* alternative approach*/
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int size=nums.size();
+        int prefix=1,suffix=1;
+        int max_product=INT_MIN;
+        for(int i=0;i<size;i++)
+        {
+            if(prefix==0) prefix=1;
+            if(suffix==0) suffix=1;
+            prefix=prefix*nums[i];
+            suffix=suffix*nums[size-1-i];
+            max_product=max(max_product,max(prefix,suffix));
+        }
+        return max_product;
+        
+    }
 };
