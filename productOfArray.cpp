66d80c3a338d9f1eaca247f5cd7f7a91838d1d2d
Meghna Singh <meghnasingh@Meghnas-MacBook-Air.local>
2024-12-11 10:03:14 +0530

    alternative approach without using division

--- productOfArray.cpp
+++ productOfArray.cpp
@@ -29,3 +29,29 @@ public:
     }
     
 };
+/* another approach*/
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int size=nums.size();
+        vector<int>prefix(size);
+        prefix[0]=1;
+        vector<int>suffix(size);
+        suffix[size-1]=1;
+        for(int i=1;i<size;i++)
+        {
+            prefix[i]=prefix[i-1]*nums[i-1];
+        }
+        for(int i=size-2;i>=0;i--)
+        {
+            suffix[i]=suffix[i+1]*nums[i+1];
+        }
+        vector<int>result(size);
+        for(int i=0;i<size;i++)
+        {
+            result[i]=prefix[i]*suffix[i];
+        }
+        return result;
+        
+    }
+};
