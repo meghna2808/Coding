89080670b67923508a6a0238f6ca08a31d43f174
meghnas2808 <meghnasingh2808@gmail.com>
2025-02-05 17:53:38 +0530

    dp

--- inorder.cpp
+++ inorder.cpp
@@ -25,3 +25,47 @@ public:
         
     }
 };
+//using stack
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(root==NULL) return {};
+        stack<TreeNode* >s;
+        s.push(root);
+        vector<int>inorder;
+        while(!s.empty())
+        {
+            TreeNode* top=s.top();
+            if(top->left==NULL)
+            {
+                inorder.push_back(top->val);
+                s.pop();
+                if(top->right)
+                {
+                    s.push(top->right);
+                    top->right=NULL;
+                }
+            }
+            else
+            {
+                s.push(top->left);
+                top->left=NULL;
+            }
+
+        }
+        return inorder;
+        
+    }
+};
+//using morris
